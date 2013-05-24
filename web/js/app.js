a418480f91d5aacb09e546ac9270c07a778b1392
Scott Markwell <scott.markwell@trustvesta.com>
2013-05-24 13:23:09 -0700

    Working on drag/drop support broken right now

--- web/js/app.js
+++ web/js/app.js
@@ -2,6 +2,10 @@ var stage;
 var text;
 var needRedraw = false;
 
+var stage_view_x = 0;
+var stage_view_y = 0;
+var stage_drag = false;
+
 // TODO
 // Add backbone.js
 // Start refactoring into logical elements
@@ -10,11 +14,11 @@ function resize_canvas()
 {
             canvas = document.getElementById("canvas");
             download = document.getElementById("download");
-            
+
                 canvas.width  = window.innerWidth;
                 canvas.height = window.innerHeight - download.offsetHeight;
-            
-            
+
+
             text.x = canvas.width/2 - text.getMeasuredWidth()/2;
        	 	text.y = canvas.height/2 - text.getMeasuredHeight()/2;
 
@@ -24,22 +28,26 @@ function handleTick(event)
 {
   if(!event.paused)
   {
+
+
     if(needRedraw)
     {
       redraw();
     }
-      
+
   }
-  
+
 }
 
 function redraw()
 {
 	resize_canvas();
+
 	stage.update();
 	//FIXME the download data should be done when the button is clicked.
   updateDownloadLink();
 	needRedraw = false;
+
 }
 
 function screenAreaChange()
@@ -51,10 +59,15 @@ function updateDownloadLink()
 {
   downloadarea = document.getElementById("download");
   canvas = document.getElementById("canvas");
-  
+
   downloadarea.innerHTML = '<a href="' + canvas.toDataURL("image/png") + '" download="YourDownloadSir.png">Download</a>';
-  
-  
+
+
+}
+
+function stageDrag(event)
+{
+
 }
 
 function init()
@@ -62,25 +75,40 @@ function init()
 	// new stage
 	stage = new createjs.Stage(document.getElementById("canvas"));
 	stage.enableMouseOver();
-	 
-	 
+
+
 	text = new createjs.Text("Drag and drop the shapes", "30px Arial", "#000000");
-	 
+
 	stage.addChild(text);
 	createjs.Ticker.setFPS(60);
 	createjs.Ticker.addEventListener("tick", handleTick);
-  
-   needRedraw = true;
-   initCircleMenu();
+
+  //createjs.Touch.enable(stage);
+
+  stage.enableMouseOver(10);
+  stage.mouseMoveOutside = true;
+
+  stage.addEventListener("stagemousedown",function(evt)  {
+      stage_drag = true;
+  })
+
+  stage.addEventListener("stagemouseup",function(evt){
+      stage_drag = false;
+  })
+
+
+  stage.addEventListener("stagemousemove", function(evt){
+          stage_view_x += evt.stageX;
+          stage_view_y += evt.stageY;
+          stage.setTranform(stage_view_x,stage_view_y);
+          needRedraw=true;
+  })
+
+
+
+  needRedraw = true;
+
 }
 
-function initCircleMenu()
-{
-   $('circle-test').circleMenu({
-      item_diameter: 40,
-      circle_radius: 100,
-      direction: 'bottom-right'
-   });
-   
-}
+
 
